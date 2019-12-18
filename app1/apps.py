b0445e36621debdc377a9cc4b2521e854ff79ba0
Faye Huang <faye1821@gmail.com>
2019-12-19 01:33:48 +0800

    Update apps.py

--- app1/apps.py
+++ app1/apps.py
@@ -8,6 +8,8 @@ class App1Config(AppConfig):
 
     def ready(self):
         # signals are imported, so that they are defined and can be used
+        # signal refs : https://segmentfault.com/a/1190000008455657
+        # signal refs : http://www.liujiangblog.com/course/django/170
         from app2.signal_handlers import app1_init_handler
         app = App1()
         app1_init.send(sender="app1", bot1=app)
